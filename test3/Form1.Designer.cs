6e23775b3be8435a7aa3e3814ae6202c335ee2b7
leopardshadow <thurtu811@gmail.com>
2016-05-06 01:39:30 +0800

    add a test red button

--- test3/Form1.Designer.cs
+++ test3/Form1.Designer.cs
@@ -284,6 +284,7 @@
             this.col12 = new System.Windows.Forms.Label();
             this.col11 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // a1
@@ -3794,12 +3795,22 @@
             this.label1.Text = "Finished!";
             this.label1.Visible = false;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(39, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 263;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(737, 588);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.col15);
             this.Controls.Add(this.col14);
@@ -4321,6 +4332,7 @@
         private System.Windows.Forms.Label col12;
         private System.Windows.Forms.Label col11;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
